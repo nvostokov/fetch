74d91d80468f38c7fed46b09e5bc5a4bda9852fe
nvostokov <103598346+nvostokov@users.noreply.github.com>
2022-11-15 23:21:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+/* let url = 'http://78.25.110.43:5566/ISAPI/Streaming/channels/402/picture?snapShotImageType=JPEG';
+
+let options = [
+    rtsp, 
+    RtSp_mdu22
+];
+
+
+let jpg = fetch(url, {
+    options,
+    headers: {
+        'Authorization': 'Basic' + btoa('${rtsp}:${RtSp}')
+    }
+});
+
+
+
+ */
+let response = fetch('http://78.25.110.43:5566/ISAPI/Streaming/channels/402/picture?snapShotImageType=JPEG', {method:'GET', 
+headers: {'Authorization': 'Basic ' + btoa('rtsp:RtSp_mdu22')}});
+
+
